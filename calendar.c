c1e1b2abd96588fca6a9b61ef1826295f3a03e6a
kellyxglobal <Kellyxglobal@gmail.com>
2022-08-27 16:33:01 -0700

    Calendar Project

--- calendar.c
+++ calendar.c
@@ -10,5 +10,19 @@
 int main() 
 {
 	int year;
+
+	//Step: Accept users input
+	printf("\nPlease, enter the year for the calenday\n");
+	scanf("%d", &year);
+
+	//Step 2: Create a character- array pointer to store all the months in a year
+	char *months = {"Jan","Mar","Feb","Apr","May","june","Jul","Aug","Sept","Oct","Nov","Dec"};
+
+	//Step 3: Display all the months in one column by passing an increament to the variable pointer array
+	for(month=0; month<12; month++)
+	{
+		printf("--------------------%s--------------------\n", months[month]);
+		printf("---Sun. Mon. Tue. Wed. Thurs. Fri. Sat---");
+	}
 }
 
