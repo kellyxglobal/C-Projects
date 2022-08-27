c71d3c369beaa6947e2e3b1d201e2d454b076d03
kellyxglobal <Kellyxglobal@gmail.com>
2022-08-27 16:09:45 -0700

    Calendar project

---
+++ calendar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * A program to print the month of the year
+ * &year = For entering the calendar year
+ * months: for getting months of the year
+ *
+ */
+ 
+int main() 
+{
+	int year;
+}
+
